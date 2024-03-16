6848a4be3b370be6c581d3569c644c2774626349
jose daniel <jdcc.corona@gmail.com>
2024-03-15 18:06:20 -0700

    cambio

--- server/index.js
+++ server/index.js
@@ -10,52 +10,16 @@ app.use(express.static("client"));
 // parse post params sent in body in json format
 app.use(express.json());
 
+const {   PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 8888} = process.env;
 
-
-//import '/dotenv/config'
-//  const client_id =process.env.PAYPAL_CLIENT_ID;
- //console.log(client_id+ "hola")
-
-//    const PAYPAL_CLIENT_SECRET=process.env.PAYPAL_CLIENT_SECRET;
-// const PAYPAL_CLIENT_ID=process.env.PAYPAL_CLIENT_SECRET;
-
- // import {PAYPAL_CLIENT_ID} from './dotenv'
-const {   PORT = 8888} = process.env;
- //const PAYPAL_CLIENT_ID='AV0X6JBqEpMvo9elZ_wXbKlewJwZjn2Z0fhmlXzhb91MuK-sqiFasEVqX-BEmvGdkToPKEGu4Pg4ERwa';
-//const PAYPAL_CLIENT_SECRET='EOHTJVwD7rQ2RmgASl1SQpZ5p1VTTDZf-2wRrPhpSXyku2aIllqTuyU_wwazycjwqAuvom0zVSFO98xu';
 const base = "https://api-m.sandbox.paypal.com";
-
-  
 
   
 /**
 * Generate an OAuth 2.0 access token for authenticating with PayPal REST APIs.
-* @see https://developer.paypal.com/api/rest/authentication/
+* @see https://developer.paypal.com/api/rest/authentication/*/
 
-*/
-// const allowCors = fn => async (req, res) => {
-//   res.setHeader('Access-Control-Allow-Credentials', true)
-//   res.setHeader('Access-Control-Allow-Origin', '*')
-//   // another common pattern
-//   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//   )
-//   if (req.method === 'OPTIONS') {
-//     res.status(200).end()
-//     return
-//   }
-//   return await fn(req, res)
-// }
 
-// const handler = (req, res) => {
-//   const d = new Date()
-//   res.end(d.toString())
-// }
-
-// module.exports = allowCors(handler)
 
 
 
@@ -67,7 +31,7 @@ const generateAccessToken = async () => {
       throw new Error("MISSING_API_CREDENTIALS");
     }
     const auth = Buffer.from(
-      process.env.PAYPAL_CLIENT_ID + ":" + process.env.PAYPAL_CLIENT_SECRET
+     PAYPAL_CLIENT_ID + ":" + PAYPAL_CLIENT_SECRET
     ).toString("base64");
     const response = await fetch(`${base}/v1/oauth2/token`, {
       method: "POST",
